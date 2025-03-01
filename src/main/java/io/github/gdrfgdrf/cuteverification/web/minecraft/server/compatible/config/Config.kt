b320db4a02b00d3ecafe5848041e5c2361e7f8d5
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2025-03-02 00:07:01 +0800

    move config

--- src/main/java/io/github/gdrfgdrf/cuteverification/web/minecraft/server/compatible/config/Config.kt
+++ src/main/java/io/github/gdrfgdrf/cuteverification/web/minecraft/server/compatible/config/Config.kt
@@ -1,13 +1,14 @@
-package io.github.gdrfgdrf.cuteverification.web.minecraft.server.compatible
+package io.github.gdrfgdrf.cuteverification.web.minecraft.server.compatible.config
+
+import com.fasterxml.jackson.annotation.JsonProperty
 
 data class Config(
     var url: String? = null,
     var username: String? = null,
-    var password: String? = null
+    var password: String? = null,
 ) {
 
     fun isValid(): Boolean {
         return !url.isNullOrBlank() && !username.isNullOrBlank() && !password.isNullOrBlank()
     }
-
 }
