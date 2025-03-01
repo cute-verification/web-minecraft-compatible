9e4588da4a02eb06c8eda98409c1a64ea16b514c
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2025-03-01 21:03:12 +0800

    override IUser's variables in IExtendedUser

--- src/main/java/io/github/gdrfgdrf/cuteverification/web/minecraft/server/compatible/IExtendedUser.kt
+++ src/main/java/io/github/gdrfgdrf/cuteverification/web/minecraft/server/compatible/IExtendedUser.kt
@@ -4,5 +4,10 @@ import io.github.gdrfgdrf.cuteverification.web.mediator.user.IUser
 import io.github.gdrfgdrf.cuteverification.web.minecraft.server.compatible.enums.KickReasons
 
 interface IExtendedUser : IUser {
+    override var id: String?
+    override var username: String
+    override var code: String
+    override var ip: String
+
     fun kick(reason: KickReasons)
 }
