746bde72e696c16a0a787fc80ffdc65993d3ca05
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2025-03-02 00:08:47 +0800

    rename UserJoinEventCaller to UserJoin

--- src/main/java/io/github/gdrfgdrf/cuteverification/web/minecraft/server/compatible/events/UserJoin.kt
+++ src/main/java/io/github/gdrfgdrf/cuteverification/web/minecraft/server/compatible/events/UserJoin.kt
@@ -3,7 +3,8 @@ package io.github.gdrfgdrf.cuteverification.web.minecraft.server.compatible.even
 import io.github.gdrfgdrf.cuteverification.web.mediator.event.user.UserJoinEvent
 import io.github.gdrfgdrf.cuteverification.web.minecraft.server.compatible.Compatible
 
-object UserJoinEventCaller {
+object UserJoin {
+
     fun call(username: String, code: String, ip: String) {
         val user = Compatible.userPool!!.findUser(username, code, ip)
         UserJoinEvent.main(user).post()
